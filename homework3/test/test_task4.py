3864c7d8e04fbcbd3b289b34e3dfb416bbfea1bc
N1ntEnd0 <nikita05012000@mail.ru>
2021-03-25 00:28:55 +0300

    -

--- homework3/test/test_task4.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from Task04 import is_armstrong
-
-
-@pytest.mark.parametrize(
-    ["value", "expected_result"], [(10, False), (9, True), (153, True), (0, True)]
-)
-def test_number(value, expected_result):
-    assert is_armstrong(value) == expected_result
