df9342669ab96954302e2673b526601c9efdac88
N1ntEnd0 <nikita05012000@mail.ru>
2021-04-07 14:58:24 +0300

    hw5

--- homework4/Test/test_Task2.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import patch
-
-import Task2
-
-
-@patch("Task2.count_dots_on_i")
-def test_count_dots(mock):
-    mock.return_value = 56
-    assert Task2.count_dots_on_i("https://example.com/") == 56
