81538b1a37be4ece6e89613511c8642fa9171d83
N1ntEnd0 <nikita05012000@mail.ru>
2021-05-19 00:02:02 +0300

    fix black

--- FinalProject/wallet_service/migrations/0003_auto_20210518_2355.py
+++ FinalProject/wallet_service/migrations/0003_auto_20210518_2355.py
@@ -9,31 +9,37 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wallet_service', '0002_auto_20210516_0100'),
+        ("wallet_service", "0002_auto_20210516_0100"),
     ]
 
     operations = [
-        migrations.DeleteModel(
-            name='Test1',
+        migrations.DeleteModel(name="Test1",),
+        migrations.AlterField(
+            model_name="transactionlogs",
+            name="amount",
+            field=models.DecimalField(
+                decimal_places=2,
+                max_digits=19,
+                validators=[django.core.validators.MinValueValidator(Decimal("0"))],
+            ),
         ),
         migrations.AlterField(
-            model_name='transactionlogs',
-            name='amount',
-            field=models.DecimalField(decimal_places=2, max_digits=19, validators=[django.core.validators.MinValueValidator(Decimal('0'))]),
-        ),
-        migrations.AlterField(
-            model_name='transactionlogs',
-            name='recipient',
+            model_name="transactionlogs",
+            name="recipient",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='transactionlogs',
-            name='sender',
+            model_name="transactionlogs",
+            name="sender",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='wallets',
-            name='balance',
-            field=models.DecimalField(decimal_places=2, max_digits=19, validators=[django.core.validators.MinValueValidator(Decimal('0'))]),
+            model_name="wallets",
+            name="balance",
+            field=models.DecimalField(
+                decimal_places=2,
+                max_digits=19,
+                validators=[django.core.validators.MinValueValidator(Decimal("0"))],
+            ),
         ),
     ]
